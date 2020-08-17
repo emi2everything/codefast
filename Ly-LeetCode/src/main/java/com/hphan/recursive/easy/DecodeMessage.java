157ada42ea10c54183a2d7a9d84c126724c02879
emi2e <emi2e@DESKTOP-0JFO24L.lan>
2020-08-17 21:11:56 +0800

    2 problems

---
+++ Ly-LeetCode/src/main/java/com/hphan/recursive/easy/DecodeMessage.java
@@ -0,0 +1,60 @@
+package com.hphan.recursive.easy;
+
+public class DecodeMessage
+{
+    public static void main(String[] args) 
+    {
+	String input = "3[a]2[bc]";
+	System.out.println(decodeString(input));
+    }
+
+    public static String decodeString(String s) 
+    {
+	StringBuilder out = new StringBuilder();
+	int i = 0;
+	while (i < s.length())
+	{
+	    String noLoop = "";
+	    
+	    if (Character.isDigit(s.charAt(i)))
+	    {
+		noLoop += s.charAt(i);
+		while(Character.isDigit(s.charAt(i+1)))
+		{
+		    noLoop+=s.charAt(i+1);
+		    i++;
+		}
+		int bIndex = i + 2;
+		int bracketCounter = 1;
+		StringBuilder bracketContent = new StringBuilder();
+
+		while (bracketCounter > 0 && bIndex < s.length())
+		{
+		    // Bracket counter
+		    if (s.charAt(bIndex) == ']')
+			bracketCounter -= 1;
+		    else if (s.charAt(bIndex) == '[')
+			bracketCounter += 1;
+
+		    // Bracket Content
+		    bracketContent.append(String.valueOf(s.charAt(bIndex)));
+		    bIndex++;
+		}
+		String bracketContentS = bracketContent.deleteCharAt(bracketContent.length() - 1).toString();
+		String backetContentDecoded = decodeString(bracketContentS);
+		Integer noLoopInt = Integer.parseInt(noLoop);
+		for (int j = 0; j < noLoopInt; j++)
+		    out.append(backetContentDecoded);
+
+		i = bIndex;
+
+	    } 
+	    else
+	    {
+		out.append(String.valueOf(s.charAt(i)));
+		i++;
+	    }
+	}
+	return out.toString();
+    }
+}
